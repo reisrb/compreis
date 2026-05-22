dc89b67bb65a12339451a3dafbd1c3c883eca32f
reisrb <rafael.reis@acordocerto.com.br>
2026-05-22 11:08:36 -0300

    fix: store Unidade as String to avoid SwiftData composite attribute error

--- ListaDeCompras/Models/Item.swift
+++ ListaDeCompras/Models/Item.swift
@@ -5,20 +5,25 @@ import SwiftData
 final class Item {
     var nome: String
     var preco: Double
-    var unidade: Unidade
+    var unidadeRaw: String
     var quantidade: Double
+
+    var unidade: Unidade {
+        get { Unidade(rawValue: unidadeRaw) ?? .unidade }
+        set { unidadeRaw = newValue.rawValue }
+    }
 
     init(nome: String, preco: Double, unidade: Unidade, quantidade: Double) {
         self.nome = nome
         self.preco = preco
-        self.unidade = unidade
+        self.unidadeRaw = unidade.rawValue
         self.quantidade = quantidade
     }
 
     var total: Double { preco * quantidade }
 }
 
-enum Unidade: String, Codable, CaseIterable {
+enum Unidade: String, CaseIterable {
     case unidade = "un"
     case kg = "kg"
 }
