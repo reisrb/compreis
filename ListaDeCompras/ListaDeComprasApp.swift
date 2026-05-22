ef125785b447839bf64eb94b758ad2f48d68a8cf
reisrb <rafael.reis@acordocerto.com.br>
2026-05-22 10:30:01 -0300

    feat: force dark mode

--- ListaDeCompras/ListaDeComprasApp.swift
+++ ListaDeCompras/ListaDeComprasApp.swift
@@ -6,6 +6,7 @@ struct ListaDeComprasApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .preferredColorScheme(.dark)
         }
         .modelContainer(for: Item.self)
     }
